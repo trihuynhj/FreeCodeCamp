cb9a87d74f47cc7fda6d9cb77324cd0d230bf84b
Tri <tri.huynh.j@gmail.com>
2021-12-03 22:15:56 +0900

    FrontEndLibraries - RandomQuoteMachine - part 17

--- FrontEndLibraries/RandomQuoteMachine/index.js
+++ FrontEndLibraries/RandomQuoteMachine/index.js
@@ -43,7 +43,7 @@ class App extends React.Component {
 
     render() {
         return (
-            <div id="wrapper">
+            <div className="wrapper">
                 Hello Again!
             </div>
         )
