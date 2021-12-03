dfe57f9aaf4a62e3e529b796208e8314d1f70d79
Tri <tri.huynh.j@gmail.com>
2021-12-03 22:04:48 +0900

    FrontEndLibraries - RandomQuoteMachine - part 11

--- FrontEndLibraries/RandomQuoteMachine/index.js
+++ FrontEndLibraries/RandomQuoteMachine/index.js
@@ -4,10 +4,23 @@
 // 4. Add event listener
 // 5. Add CSS styling
 
-function App() {
-    return (
-        <div>Hello World!</div>
-    )
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quotes: [],
+            index: 0,
+            colorIndex: 0
+        }
+    }
+
+    render() {
+        return (
+            <div id="wrapper">
+                Hello Again!
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
