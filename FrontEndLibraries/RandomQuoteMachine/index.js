3a496c0494bbcb8dea3d36925dcd2c397543652f
Tri <tri.huynh.j@gmail.com>
2021-12-03 22:07:52 +0900

    FrontEndLibraries - RandomQuoteMachine - part 12

--- FrontEndLibraries/RandomQuoteMachine/index.js
+++ FrontEndLibraries/RandomQuoteMachine/index.js
@@ -4,6 +4,9 @@
 // 4. Add event listener
 // 5. Add CSS styling
 
+// API to get the JSON quotes
+const quoteAPI = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -12,6 +15,14 @@ class App extends React.Component {
             index: 0,
             colorIndex: 0
         }
+    }
+
+    componentDidMount() {
+        fetch(quoteAPI)
+            .then(response => response.json())
+            .then(data => {
+                this.setState({quotes: data.quotes}, this.getRandomIndex);
+            })
     }
 
     render() {
