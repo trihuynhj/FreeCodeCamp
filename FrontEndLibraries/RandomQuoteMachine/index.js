e1f19744b320734aca1eb24f628bf0a9c432245d
Tri <tri.huynh.j@gmail.com>
2021-12-04 19:50:45 +0900

    FrontEndLibraries - RandomQuoteMachine - part 18

--- FrontEndLibraries/RandomQuoteMachine/index.js
+++ FrontEndLibraries/RandomQuoteMachine/index.js
@@ -7,6 +7,30 @@
 // API to get the JSON quotes
 const quoteAPI = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
+// Color palette
+const txtCol = [
+    "#2c6e49", // Spring Green
+    "#a60067", // Flirt
+    "#6153cc", // Iris
+    "#961d4e", // Jazzberry Jam
+    "#0d2c54", // Prussian Blue
+    "#f6511d", // Red Orange Color Wheel
+    "#ffb400", // Honey Yellow
+    "#4d4861", // Independence
+    "#de541e", // Flame
+    "#3f3f37", // Black Olive
+    "#6e4555", // Eggplant
+    "#3a3238", // Black Coffee
+    "#000022", // Oxford Blue
+    "#dd1c1a", // Maximum Red
+    "#3f6c51", // Amazon
+    "#157a6e", // Pine Green
+    "#b56576", // Rose Dust
+    "#e56b6f", // Candy Pink
+    "#a63a50", // Amaranth Purple
+    "#582b11"  // Seal Brown
+  ];
+
 class App extends React.Component {
     constructor(props) {
         super(props);
